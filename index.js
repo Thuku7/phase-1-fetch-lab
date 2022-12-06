0ffccb95c46bfb19af8b088ac79c33668064beae
Robert <robethuku11@gmail.com>
2022-12-06 07:33:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(books => renderBooks(books))
   
 }
 
@@ -13,5 +17,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks() ;
 });
